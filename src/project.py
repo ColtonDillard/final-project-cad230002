8621359c53091ed77ec689059fd727c26a482d5a
Colton Dillard <cad230002@utdallas.edu>
2024-11-10 16:08:30 -0600

    Created FindShadows function which will isolate pixels within a certain range

--- src/project.py
+++ src/project.py
@@ -9,14 +9,22 @@ class ShadowForge():
             img_gray.show()
     
 
-    def function_n():
-        print()
+    def FindShadows(self, img):
+        img_shadow = img.copy()
+        for y in range(img.height):
+            for x in range(img.width):
+                pix = img.getpixel((x,y))
+                if pix <= 105:
+                    img_shadow.putpixel((x,y), (0,0,255,255))
+                else:
+                    img_shadow.putpixel((x,y), (255,255,255,255))
     
 
 def main():
     sf = ShadowForge()
     #img = sf.GetGrayscale()
-    sf.GetGrayscale()
+    img_gray = sf.GetGrayscale()
+    sf.FindShadows(img_gray)
     
 
 if __name__ == "__main__":
