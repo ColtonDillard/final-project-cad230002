402726b7ef5ebe68ed46ff736074271ff15e5e1e
Colton Dillard <cad230002@utdallas.edu>
2024-11-10 15:45:21 -0600

    Created class and get greyscale function as well as added an image

--- src/project.py
+++ src/project.py
@@ -1,18 +1,23 @@
+from PIL import Image
+
+class ShadowForge():
+
+    def GetGrayscale(self):
+        with Image.open('test.png') as img:
+            img_gray = img.copy()
+            img_gray = img.convert('L')
+            img_gray.show()
+    
+
+    def function_n():
+        print()
+    
+
 def main():
-    ...
-
-
-def function_1():
-    ...
-
-
-def function_2():
-    ...
-
-
-def function_n():
-    ...
-
+    sf = ShadowForge()
+    #img = sf.GetGrayscale()
+    sf.GetGrayscale()
+    
 
 if __name__ == "__main__":
     main()
